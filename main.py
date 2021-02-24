a5c277aa492461600e0e72c33d19debd6387360a
definedhorizons <petya.potapenko@gmail.com>
2021-02-24 10:28:54 +0300

    added buildozer.spec

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.scatterlayout import ScatterLayout
 
 
 class CorporationGame(Widget):
